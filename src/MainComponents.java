3c0ead9ba8312c0d3b369ac7428b67101a11b092
ikostan <superegorius@gmail.com>
2017-08-23 23:44:29 -0700

    MainComponents - all GUI objects are ready

--- src/MainComponents.java
+++ src/MainComponents.java
@@ -4,15 +4,27 @@ import javax.swing.JPanel;
 import java.awt.BorderLayout;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 import javax.swing.JRadioButton;
+import javax.swing.JCheckBox;
+import javax.swing.JTextField;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import javax.swing.JSlider;
 
 public class MainComponents extends JFrame{
 	
 	private JPanel panel;
-	private JLabel lblMainSwingComponents, lblRadioGroups;
+	private JLabel lblMainSwingComponents, lblRadioGroups, lblUserId, lblPswd;
 	private ButtonGroup radioGrp;
-	private JRadioButton radioButton1, radioButton2, radioButton3;
-	
+	private JRadioButton radioThue, radioFri, radioSat;
+	private JCheckBox chckbxRed, chckbxGreen, chckbxBlue;
+	private JLabel lblColorChoices, lblSlider;
+	private JTextField textID, textPWD;
+	private JButton btnSubmit;
+	private JSlider slider;
 	
 	//Constructor
 	public MainComponents() {
@@ -20,6 +32,15 @@ public class MainComponents extends JFrame{
 		setMainWindow();
 		setWindowTitle();
 		setRadioGrp();
+		setColorChoices();
+		setUserID();
+		setUserPswd();
+		setSliderObj();
+		setSubmitBtn();
+		
+		this.revalidate();
+		this.repaint();
+		this.setVisible(true);
 	}
 
 	
@@ -28,11 +49,10 @@ public class MainComponents extends JFrame{
 		this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		this.setTitle("Main Components");
 		this.setResizable(false);
-		this.setSize(400, 300);
+		this.setSize(400, 370);
 		this.panel = new JPanel();
 		panel.setLayout(null);
 		this.getContentPane().add(panel, BorderLayout.CENTER);
-		this.setVisible(true);
 	}
 	
 	
@@ -58,22 +78,102 @@ public class MainComponents extends JFrame{
 		
 		setRadioTitle();
 
-		radioButton1 = new JRadioButton("Thursday");
-		radioButton1.setBounds(26, 99, 109, 23);
-		panel.add(radioButton1);
+		radioThue = new JRadioButton("Thursday");
+		radioThue.setBounds(26, 99, 109, 23);
+		panel.add(radioThue);
 		
-		radioButton2 = new JRadioButton("Friday");
-		radioButton2.setBounds(26, 125, 109, 23);
-		panel.add(radioButton2);
+		radioFri = new JRadioButton("Friday");
+		radioFri.setBounds(26, 125, 109, 23);
+		panel.add(radioFri);
 		
-		radioButton3 = new JRadioButton("Saturday");
-		radioButton3.setBounds(26, 151, 109, 23);
-		panel.add(radioButton3);
+		radioSat = new JRadioButton("Saturday");
+		radioSat.setBounds(26, 151, 109, 23);
+		panel.add(radioSat);
 		
 		radioGrp = new ButtonGroup();
-		radioGrp.add(radioButton1);
-		radioGrp.add(radioButton2);
-		radioGrp.add(radioButton3);
+		radioGrp.add(radioThue);
+		radioGrp.add(radioFri);
+		radioGrp.add(radioSat);
+	}
+	
+	
+	private void setColorChoices(){
+		
+		lblColorChoices = new JLabel("Color Choices:");
+		lblColorChoices.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblColorChoices.setBounds(228, 66, 89, 20);
+		panel.add(lblColorChoices);
+		
+		chckbxRed = new JCheckBox("Red");
+		chckbxRed.setBounds(220, 99, 97, 23);
+		panel.add(chckbxRed);
+		
+		chckbxGreen = new JCheckBox("Green");
+		chckbxGreen.setBounds(220, 125, 97, 23);
+		panel.add(chckbxGreen);
+		
+		chckbxBlue = new JCheckBox("Blue");
+		chckbxBlue.setBounds(220, 151, 97, 23);
+		panel.add(chckbxBlue);
+	}
+	
+	
+	private void setUserID(){
+		
+		lblUserId = new JLabel("User ID:");
+		lblUserId.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblUserId.setBounds(230, 202, 46, 14);
+		panel.add(lblUserId);
+		
+		textID = new JTextField();
+		textID.setBounds(286, 199, 86, 20);
+		panel.add(textID);
+		textID.setColumns(10);
+	}
+	
+	
+	private void setUserPswd(){
+		
+		lblPswd = new JLabel("Password:");
+		lblPswd.setHorizontalAlignment(SwingConstants.RIGHT);
+		lblPswd.setBounds(212, 233, 64, 14);
+		panel.add(lblPswd);
+		
+		textPWD = new JTextField();
+		textPWD.setBounds(286, 230, 86, 20);
+		panel.add(textPWD);
+		textPWD.setColumns(10);
+	}
+	
+	
+	private void setSliderObj(){
+		
+		lblSlider = new JLabel("Slider:");
+		lblSlider.setFont(new Font("Tahoma", Font.PLAIN, 13));
+		lblSlider.setBounds(27, 198, 46, 20);
+		panel.add(lblSlider);
+		
+		slider = new JSlider();
+		slider.setBounds(10, 224, 200, 26);
+		panel.add(slider);		
+	}	
+	
+	
+	private void setSubmitBtn(){
+		
+		btnSubmit = new JButton("SUBMIT");
+		btnSubmit.setBounds(26, 295, 89, 23);
+		btnSubmit.addActionListener(new ActionListener(){
+
+			@Override
+			public void actionPerformed(ActionEvent arg0) {
+				// TODO Auto-generated method stub
+				
+			}
+			
+			
+		});
+		panel.add(btnSubmit);
 	}
 	
 	
